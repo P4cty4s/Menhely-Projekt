bcce10ddfccb002e1861c88666170244184f4c93
P4cty4s <ttimpauer@gmail.com>
2024-11-25 16:03:45 +0100

    Initial commit

---
+++ Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Menhely_Projekt.Models
+{
+    internal class User
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public User(int _id, string _name, string _password)
+        {
+            ID = _id;
+            Name = _name;
+            Password = _password;
+        }
+    }
+}
---
+++ UserDAO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Menhely_Projekt.Models;
+using MySql.Data.MySqlClient;
+
+namespace Menhely_Projekt
+{
+    public static class UserDAO
+    {
+        public static int login(string _name,string _password)
+        {
+            int _id = -1;
+
+            string connectionString = "datasource=localhost;port=3306;username=root;password=;database=zoldmenedek";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+                string query = "SELECT id FROM users WHERE username = @name AND password = @password";
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@name", _name);
+                    command.Parameters.AddWithValue("@password", _password);
+                    using (MySqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            reader.Read();
+                            _id = reader.GetInt32(0);
+                        }
+                    }
+                }
+                connection.Close();
+            }
+            
+            return _id;
+
+        }
+    }
+}
